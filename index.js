7575488172a5ac8646df7a256f953576c9b03c51
Allen Byerly <allenbsf@gmail.com>
2015-10-31 15:47:09 -0700

    nah

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var config = require('./lib/config');
 
 app.listen(config.serverPort, function () {
   console.log('App is listening at "http://0.0.0.0:%s" in the "%s" environment.', config.serverPort, config.environment);
-});
+}) ;
 
